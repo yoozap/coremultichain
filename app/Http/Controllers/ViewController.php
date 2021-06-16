e86d581862badf12228610cca2169a4072e50ee6
sergo93@gmail.com <sergo93@gmail.com>
2021-06-16 12:42:09 +0400

    last

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ViewController extends Controller {
+    public function app() {
+        return view('app');
+    }
+}
---
+++ public/precache-manifest.afa6d44768798c7754f366b9f07e9882.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24e50a0c673b8502a26",
+    "url": "/css/app.9c164754.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7baa1fef987bc9b6a0a6c9c9a5c658f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f06759fc196341828778",
+    "url": "/js/about.51346cb8.js"
+  },
+  {
+    "revision": "e24e50a0c673b8502a26",
+    "url": "/js/app.40b7dc06.js"
+  },
+  {
+    "revision": "eaa3b10a8adb3b8c79dc",
+    "url": "/js/chunk-vendors.6cad2dfe.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
