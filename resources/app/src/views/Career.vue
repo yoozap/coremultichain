73e555c200b946fba636332d37c97ece67a525c8
Ioseliani <tak.ioseliani@gmail.com>
2021-06-17 00:58:14 +0400

    start career page

---
+++ resources/app/src/views/Career.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="body-container">
+        <CareerMainContainer />
+        <CareerValues />
+        <!-- <Ineligibility />
+    <Rewards />
+    <ReportBug /> -->
+    </div>
+</template>
+
+<script>
+import CareerMainContainer from "@/components/CareerMainContainer.vue";
+import CareerValues from "@/components/CareerValues.vue";
+// import Ineligibility from "@/components/Ineligibility.vue";
+// import Rewards from "@/components/Rewards.vue";
+// import ReportBug from "@/components/ReportBug.vue";
+
+export default {
+    name: "BugBounty",
+    components: {
+        CareerMainContainer,
+        CareerValues
+        // Ineligibility,
+        // Rewards,
+        // ReportBug
+    }
+};
+</script>
+<style scoped>
+.body-container {
+    padding-left: 210px;
+}
+/*Ipad 768*/
+@media (max-width: 1023px) {
+    .body-container {
+        padding-left: 180px;
+    }
+}
+/*Mobile 320*/
+@media (max-width: 767px) {
+    .body-container {
+        padding-left: 0px;
+    }
+}
+</style>
